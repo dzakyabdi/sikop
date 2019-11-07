4109cfde4fc087dc13ad32a19e6e611042369878
Mohammad Wildan Yanuar <mohammadwildanyanuar@yahoo.co.id>
2019-11-07 22:37:13 +0700

    database/SimpananModel & JenisSimpananModel

--- sikoperasi/src/main/java/apap/tugasakhir/sikoperasi/model/JenisSimpananModel.java
+++ sikoperasi/src/main/java/apap/tugasakhir/sikoperasi/model/JenisSimpananModel.java
@@ -1,4 +1,23 @@
 package apap.tugasakhir.sikoperasi.model;
 
-public class JenisSimpananModel {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "jenis_simpanan")
+public class JenisSimpananModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 200)
+    @Column(name="nama", nullable = false)
+    private String nama;
+
+    @OneToMany(mappedBy = "simpanan", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<SimpananModel> simpananModelList;
 }
--- sikoperasi/src/main/java/apap/tugasakhir/sikoperasi/model/SimpananModel.java
+++ sikoperasi/src/main/java/apap/tugasakhir/sikoperasi/model/SimpananModel.java
@@ -1,4 +1,46 @@
 package apap.tugasakhir.sikoperasi.model;
 
-public class SimpananModel {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "simpanan")
+public class SimpananModel implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name="tanggal_setor", nullable = false)
+    private Date tanggal_setor;
+
+    @NotNull
+    @Column(name="jumlah", nullable = false)
+    private Integer jumlah;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "id_jenis_simpanan", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private JenisSimpananModel id_jenis_simpanan;
+
+//    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+//    @JoinColumn(name = "id_anggota_penyetor", referencedColumnName = "id", nullable = false)
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    @JsonIgnore
+//    private JenisSimpananModel id_anggota_penyetor;
+//
+//    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+//    @JoinColumn(name = "id_anggota_penerima", referencedColumnName = "id", nullable = false)
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    @JsonIgnore
+//    private JenisSimpananModel id_anggota_penerima;
 }
