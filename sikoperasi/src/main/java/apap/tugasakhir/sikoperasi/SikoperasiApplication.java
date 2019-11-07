4154769b26b77a36d8081cd7c49307994cabc616
Dzaky Abdi Al Jabbar <dzakyabdi17@gmail.com>
2019-11-07 13:51:49 +0700

    Add spring init: sikoperasi

---
+++ sikoperasi/src/main/java/apap/tugasakhir/sikoperasi/SikoperasiApplication.java
@@ -0,0 +1,13 @@
+package apap.tugasakhir.sikoperasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikoperasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikoperasiApplication.class, args);
+	}
+
+}
