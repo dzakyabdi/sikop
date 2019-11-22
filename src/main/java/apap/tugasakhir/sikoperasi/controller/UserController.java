ec9adf768dc091aa9715bac5be49bca669d3b5c2
Dzaky Abdi Al Jabbar <33620623+dzakyabdi@users.noreply.github.com>
2019-11-22 21:44:56 +0700

    Feat/dzaky (#37)
    * Add Feat:Detail Anggota by Dzaky
    
    * add fitur:Menambah Anggota
    
    * add fitur:Menambah Anggota(2)
    
    * Add Detail Anggota
    
    * punya gua
    
    * Add Menambah User
    
    * bismillah
    
    * post user siswa berhasil

--- src/main/java/apap/tugasakhir/sikoperasi/controller/UserController.java
+++ src/main/java/apap/tugasakhir/sikoperasi/controller/UserController.java
@@ -1,14 +1,11 @@
 package apap.tugasakhir.sikoperasi.controller;
 
+import apap.tugasakhir.sikoperasi.model.AnggotaModel;
 import apap.tugasakhir.sikoperasi.model.UserModel;
-import apap.tugasakhir.sikoperasi.rest.ResponseDetail;
+import apap.tugasakhir.sikoperasi.service.AnggotaService;
 import apap.tugasakhir.sikoperasi.rest.UserDetail;
 import apap.tugasakhir.sikoperasi.service.UserRestService;
 import apap.tugasakhir.sikoperasi.service.UserService;
-import reactor.core.publisher.Mono;
-
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -16,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Random;
 
 @Controller
 @RequestMapping("/user")
@@ -27,14 +26,42 @@ public class UserController {
     @Autowired
     private UserRestService userRestService;
 
+    @Autowired
+    private AnggotaService anggotaService;
+
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     private String addUserSumbit(
             RedirectAttributes attributes,
-            @ModelAttribute UserModel user
+            @ModelAttribute UserModel user, AnggotaModel anggota
     ) {
         Boolean passwordChecker = userService.passwordRegexChecker(user.getPassword());
         if (passwordChecker) {
+
             userService.addUser(user);
+            String nia = "";
+            Random random = new Random();
+
+            for(int i = 0; i < 8; i++) {
+                int x = random.nextInt(10);
+                nia += String.valueOf(x);
+            }
+            anggota.setNia(nia);
+            anggota.setKtp(anggota.getKtp());
+            anggota.setUser(user);
+            anggotaService.addAnggota(anggota);
+
+
+//            userService.addUser(user);
+            if(user.getRole().getNama().equals("Kepala Sekolah") || user.getRole().getNama().equals("Guru")) {
+                userRestService.postUserGuruToSiSivitas(userRestService.postGuruDetail(user, anggota));
+            }
+            else if (user.getRole().getNama().equals("Admin TU") || user.getRole().getNama().equals("Pustakawan")
+                    || user.getRole().getNama().equals("Pengurus Koperasi")
+                    || user.getRole().getNama().equals("Anggota Koperasi")) {
+                userRestService.postUserPegawaiToSiSivitas(userRestService.postPegawaiDetail(user, anggota));
+            }
+
+            userRestService.postUserSiswaToSiSivitas(userRestService.postSiswaDetail(user, anggota));
             return "redirect:/";
         }
         String message = "Password harus terdiri dari setidaknya 1 huruf dan 1 angka dan terdiri dari setidaknya 8 karakter";
--- src/main/java/apap/tugasakhir/sikoperasi/service/UserRestServiceImpl.java
+++ src/main/java/apap/tugasakhir/sikoperasi/service/UserRestServiceImpl.java
@@ -1,32 +1,152 @@
 package apap.tugasakhir.sikoperasi.service;
 
-import java.util.Map;
-
-import javax.transaction.Transactional;
-
+import apap.tugasakhir.sikoperasi.model.AnggotaModel;
+import apap.tugasakhir.sikoperasi.model.UserModel;
+import apap.tugasakhir.sikoperasi.repository.UserDB;
+import apap.tugasakhir.sikoperasi.rest.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import apap.tugasakhir.sikoperasi.rest.ResponseDetail;
-import apap.tugasakhir.sikoperasi.rest.Setting;
 import reactor.core.publisher.Mono;
 
+import java.text.SimpleDateFormat;
+import java.util.Random;
+
 @Service
-@Transactional
 public class UserRestServiceImpl implements UserRestService{
-	private final WebClient webClient;
-	
-	public UserRestServiceImpl(WebClient.Builder webClientBuilder) {
-		this.webClient = webClientBuilder.baseUrl(Setting.sivitasUrl).build();
-	}	
-	
-	@Override
-	public Mono<ResponseDetail> getEmployee(String id){
-		return this.webClient.get().uri("/employees/" + id).retrieve().bodyToMono(ResponseDetail.class);
-	}
-	
-	@Override
-	public Mono<ResponseDetail> getTeacher(String id){
-		return this.webClient.get().uri("/teachers/" + id).retrieve().bodyToMono(ResponseDetail.class);
-	}
+    private final WebClient webClient;
+
+    @Autowired
+    private UserDB userDB;
+
+    public UserRestServiceImpl(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(Setting.userUrl).build();
+    }
+
+    @Override
+    public Mono<PegawaiDetail> postUser(PegawaiDetail user) {
+        return this.webClient.post().uri("/employees")
+                .syncBody(user)
+                .retrieve()
+                .bodyToMono(PegawaiDetail.class);
+    }
+
+    @Override
+    public PegawaiDetail postUserPegawaiToSiSivitas(PegawaiDetail pegawai) {
+        return this.webClient
+                .post()
+                .uri("/employees")
+                .syncBody(pegawai)
+                .retrieve()
+                .bodyToMono(PegawaiDetail.class)
+                .block();
+    }
+
+    @Override
+    public GuruDetail postUserGuruToSiSivitas(GuruDetail guru) {
+        return this.webClient
+                .post()
+                .uri("/teachers")
+                .syncBody(guru)
+                .retrieve()
+                .bodyToMono(GuruDetail.class)
+                .block();
+    }
+
+    @Override
+    public SiswaDetail postUserSiswaToSiSivitas(SiswaDetail siswa) {
+        return this.webClient
+                .post()
+                .uri("/students")
+                .syncBody(siswa)
+                .retrieve()
+                .bodyToMono(SiswaDetail.class)
+                .block();
+    }
+
+
+    @Override
+    public PegawaiDetail postPegawaiDetail(UserModel user, AnggotaModel anggota) {
+        PegawaiDetail postUser = new PegawaiDetail();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        postUser.setIdUser(user.getId());
+        postUser.setNip(generateNomorInduk(anggota,user));
+        postUser.setNama(anggota.getNama());
+        postUser.setTempatLahir(anggota.getTempat_lahir());
+        postUser.setTanggalLahir(formatter.format(anggota.getTanggal_lahir()));
+        postUser.setAlamat(anggota.getAlamat());
+        postUser.setTelepon(anggota.getNomor_telepon());
+
+        return postUser;
+    }
+
+    @Override
+    public GuruDetail postGuruDetail(UserModel user, AnggotaModel anggota) {
+        GuruDetail postUser = new GuruDetail();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        postUser.setIdUser(user.getId());
+        postUser.setNig(generateNomorInduk(anggota,user));
+        postUser.setNama(anggota.getNama());
+        postUser.setTempatLahir(anggota.getTempat_lahir());
+        postUser.setTanggalLahir(formatter.format(anggota.getTanggal_lahir()));
+        postUser.setAlamat(anggota.getAlamat());
+        postUser.setTelepon(anggota.getNomor_telepon());
+
+        return postUser;
+    }
+
+    @Override
+    public SiswaDetail postSiswaDetail(UserModel user, AnggotaModel anggota) {
+        SiswaDetail postUser = new SiswaDetail();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+        postUser.setIdUser(user.getId());
+        postUser.setNis(generateNomorInduk(anggota,user));
+        postUser.setNama(anggota.getNama());
+        postUser.setTempatLahir(anggota.getTempat_lahir());
+        postUser.setTanggalLahir(formatter.format(anggota.getTanggal_lahir()));
+        postUser.setAlamat(anggota.getAlamat());
+        postUser.setTelepon(anggota.getNomor_telepon());
+
+        return postUser;
+    }
+
+    @Override
+    public String generateNomorInduk(AnggotaModel anggota, UserModel user) {
+        String nomorInduk = user.getRole().getNama().substring(0,1);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyy");
+        nomorInduk += formatter.format(anggota.getTanggal_lahir());
+
+        Random random = new Random();
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for(int i = 0; i < 2; i++) {
+            nomorInduk += alphabet.charAt(random.nextInt(alphabet.length()));
+        }
+
+        for(int i = 0; i < 3; i++) {
+            int x = random.nextInt(10);
+            nomorInduk += String.valueOf(x);
+        }
+
+        nomorInduk += user.getId();
+
+        System.out.println(nomorInduk);
+
+        return nomorInduk;
+    }
+
+    @Override
+    public Mono<ResponseDetail> getEmployee(String id){
+        return this.webClient.get().uri("/employees/" + id).retrieve().bodyToMono(ResponseDetail.class);
+    }
+
+    @Override
+    public Mono<ResponseDetail> getTeacher(String id){
+        return this.webClient.get().uri("/teachers/" + id).retrieve().bodyToMono(ResponseDetail.class);
+    }
+
 }
