43d8ff69774ec63dfbbe7a721af9beb289985df6
Fivi Melinda <fivimelinda@gmail.com>
2019-12-06 11:04:22 +0700

    fixing navbar

--- src/main/java/apap/tugasakhir/sikoperasi/controller/PinjamanController.java
+++ src/main/java/apap/tugasakhir/sikoperasi/controller/PinjamanController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
